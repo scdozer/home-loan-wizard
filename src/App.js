602ecf9364b70f2632fe95446087c92f3fd5cbf4
Scott Cardoza <Scott@Scotts-MacBook-Pro.local>
2017-10-07 13:44:00 -0600

    Updated app.js

--- src/App.js
+++ src/App.js
@@ -5,37 +5,6 @@ import { connect } from 'react-redux';
 
 
 class App extends Component {
-  constructor(){
-    super()
-    this.state = {
-      //first view
-      loanType: 'Home Purchase',
-      propertyType: 'Single Family Home',
-      //second view
-      city: '',
-      //third view
-      propToBeUsedOn: '',
-      //fourth view
-      found: "false",
-      //fifth view
-      realEstateAgent: "false",
-      //sixth view
-      downPayment: 0,
-      cost: 0,
-      //seventh view
-      credit: '',
-      //eigth view
-      history: '',
-      //ninth view
-      addressOne: '',
-      addressTwo: '',
-      addressThree: '',
-      //tenth view
-      firstName: '',
-      lastName: '',
-      email: ''
-    }
-  }
   render() {
     return (
       <div>
